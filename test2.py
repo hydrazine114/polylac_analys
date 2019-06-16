03c8bca84aac9bcd64dbd7bd31b004fac14fba0f
hydrazine114 <hydrazine114@gmail.com>
2019-06-16 21:18:20 +0300

    from pc

--- test2.py
+++ test2.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-a = np.load('distances/structure_0.npy')
-a = np.sort(a)[:3000]
-a, bins = np.histogram(a, bins=50)
-plt.hist(bins[:-1], bins, weights=a)
-plt.show()
+size = 2048
+ind = lambda i1, i2: i1 * (size - 1) - np.sum([x for x in range(i1 + 1)]) + i2 - 1
 
+
+def showhist(a):
+    a = np.sort(a)[:3000]
+    a, bins = np.histogram(a, bins=50)
+    plt.hist(bins[:-1], bins, weights=a)
+    plt.show()
+
+
+a = np.load('distances/structure_0.npy')
+# showhist(a)
--- test3.py
+++ test3.py
@@ -1,9 +1,14 @@
 import numpy as np
-size = 10
-d = np.array([np.array(range(size)) + x for x in range(0, size ** 2, size)])
-print(d)
-d = d[np.triu_indices(size, k=1)]
-i1 = 6
-i2 = 9
-n = i1 * (size - 1) - np.sum([x for x in range(i1+1)]) + i2 - 1
-print(d[n])
+# C O H
+with open('E:\\lammpcalc\\polylac\\1_100\\1_100_5.dump') as file:
+    count = 0
+    atom = []
+    for line in file:
+        if 'Time' not in line and '2087' not in line:
+            atom.append(line[0])
+            count += 1
+            if count == 2087:
+                break
+
+print(atom[2080:])
+
