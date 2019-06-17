025839bdceeedcbb01ed4861e5a13e2f5a2c54ca
hydrazine114 <hydrazine114@gmail.com>
2019-06-17 18:33:36 +0300

    from pc

--- test3.py
+++ test3.py
@@ -5,10 +5,18 @@ with open('E:\\lammpcalc\\polylac\\1_100\\1_100_5.dump') as file:
     atom = []
     for line in file:
         if 'Time' not in line and '2087' not in line:
-            atom.append(line[0])
+            atom.append(int(line[0]))
             count += 1
             if count == 2087:
                 break
-
-print(atom[2080:])
-
+oxygen = []
+carbo = []
+for index, value in enumerate(atom):
+    if value == 1:
+        carbo.append(index)
+    elif value == 2:
+        oxygen.append(index)
+oxygen = np.array(oxygen)
+carbo = np.array(carbo)
+np.save('oxygen.npy', oxygen)
+np.save('carbo.npy', carbo)
